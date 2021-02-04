e023e1cda23662b0eef3efd7605ee13903407a8c
T2SU <seria0no@gmail.com>
2021-02-05 00:33:05 +0900

    last fix 2

--- mandatory/ft_strdup.s
+++ mandatory/ft_strdup.s
@@ -31,34 +31,30 @@
 ;	{
 ;		char			*src; // [rbp-8]
 ;		char			*buf; // [rbp-10h]
-;		size_t			len; // [rbp-18h]
 ;
 ;		src = s1;
 ;		$1 = ft_strlen(s1);
-;		len = $1;
-;		$2 = malloc(len + 1);
+;		$2 = malloc($1 + 1);
 ;		if (!$2)
 ;			return (0);
 ;		buf = $2;
 ;		return (ft_strcpy(buf, src));
 ;	}
 ;
-_ft_strdup:	push	rcx					; [ 8] char *src;
-			push	rbx					; [10] char *buf;
-			mov		rcx, rdi			; [18] src = s1;
-			call	_ft_strlen			; [18] ft_strlen(s1);
-			mov		rdi, rax			; [18]
-			inc		rdi					; [18]
-			sub		rsp, 8				; [18]
-			call	_malloc				; [20] malloc(len + 1)
-			add		rsp, 8				; [20]
-			test	rax, rax			; [18] if (!$?)
-			je		_return				; [18] goto _return;
-			mov		rbx, rax			; [18] buf = $?;
-			mov		rdi, rax			; [18]
-			mov		rsi, rcx			; [18]
-			call	_ft_strcpy			; [18] ft_strcpy(buf, src);
-_return:	mov		rax, rbx			; [18]
-			pop		rbx					; [10]
-			pop		rcx					; [ 8]
-			ret							; [ 8]
+_ft_strdup:	push	rbp					; stack frame
+			mov		rbp, rsp
+			sub		rsp, 10h			; char *src, *buf;
+			mov		[rbp-8], rdi		; src = s1;
+			call	_ft_strlen			; ft_strlen(s1);
+			mov		rdi, rax
+			inc		rdi
+			call	_malloc				; malloc(len + 1)
+			test	rax, rax			; if (!$?)
+			je		_return				; goto _return;
+			mov		[rbp-10h], rax		; buf = $?;
+			mov		rdi, rax
+			mov		rsi, [rbp-8]
+			call	_ft_strcpy			; ft_strcpy(buf, src);
+_return:	mov		rsp, rbp
+			pop		rbp
+			ret
