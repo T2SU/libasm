b599982468afb3a6e87c5b7bae710325da063733
T2SU <seria0no@gmail.com>
2021-02-05 18:12:32 +0900

    remove stack frame.

--- mandatory/ft_strcmp.s
+++ mandatory/ft_strcmp.s
@@ -13,16 +13,13 @@
 			global	_ft_strcmp
 			extern	_ft_strlen
 			section	.text
-_ft_strcmp:	push	rbp				; stack frame
-			mov		rbp, rsp
-			sub		rsp, 10h
-			mov		[rbp-8], rdi	; backup s1
-			mov		[rbp-10h], rsi	; backup s2
+_ft_strcmp:	push	rdi				; backup s1
+			push	rsi				; backup s2
 			call	_ft_strlen		; get s1's string length
 			inc		rax				; include NULL character
 			mov		rcx, rax		; max repeat counter (rcx)
-			mov		rdi, [rbp-8]	; restore s1
-			mov		rsi, [rbp-10h]	; restore s2
+			pop		rsi				; restore s2
+			pop		rdi				; restore s1
 			cld						; clear direction flag (to forward)
 			repe	cmpsb			; repeat while byte[$rsi] == byte[$rdi] and rcx != 0
 			dec		rdi
@@ -30,6 +27,4 @@ _ft_strcmp:	push	rbp				; stack frame
 			movzx	rax, byte [rdi]
 			movzx	rdx, byte [rsi]
 			sub		rax, rdx		; *s1 - *s2
-_return:	mov		rsp, rbp		; restore stack frame
-			pop		rbp
-			ret
+_return:	ret
